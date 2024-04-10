5dfd0c442f014c83ad55e054ed078a176a39f376
pushpak1497 <pushpakvyas1497@gmail.com>
2024-04-10 04:50:25 +0000

    first commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,39 +1,44 @@
+import {Component} from 'react'
 import {Link} from 'react-router-dom'
 import './index.css'
 import WebPanelContext from '../../context/WebPanelContext'
 
-const Header = () => (
-  <WebPanelContext.Consumer>
-    {value => {
-      const {searchInput, changeSearch} = value
-      const onChangeInput = event => {
-        changeSearch(event.target.value)
-      }
+class Header extends Component {
+  render() {
+    return (
+      <WebPanelContext.Consumer>
+        {value => {
+          const {searchInput, changeSearch} = value
+          const onChangeInput = event => {
+            changeSearch(event.target.value)
+          }
 
-      return (
-        <div className="header-container">
-          <p className="logo">MovieDb</p>
-          <div className="header-links-container">
-            <Link to="/" className="link">
-              <p className="header-text">Popular</p>
-            </Link>
-            <Link to="/top-rated" className="link">
-              <p className="header-text">Top Rated</p>
-            </Link>
-            <Link to="/upcoming" className="link">
-              <p className="header-text">Upcoming</p>
-            </Link>
-            <input
-              type="search"
-              value={searchInput}
-              className="input-search"
-              placeholder="Search Name"
-              onChange={onChangeInput}
-            />
-          </div>
-        </div>
-      )
-    }}
-  </WebPanelContext.Consumer>
-)
+          return (
+            <div className="header-container">
+              <p className="logo">MovieDb</p>
+              <div className="header-links-container">
+                <Link to="/" className="link">
+                  <p className="header-text">Popular</p>
+                </Link>
+                <Link to="/top-rated" className="link">
+                  <p className="header-text">Top Rated</p>
+                </Link>
+                <Link to="/upcoming" className="link">
+                  <p className="header-text">Upcoming</p>
+                </Link>
+                <input
+                  type="search"
+                  value={searchInput}
+                  className="input-search"
+                  placeholder="Search Name"
+                  onChange={onChangeInput}
+                />
+              </div>
+            </div>
+          )
+        }}
+      </WebPanelContext.Consumer>
+    )
+  }
+}
 export default Header
